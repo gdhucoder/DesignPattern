0531f57b5177e3fa3076cc8d79581f85ef3b8694
gdhu <guodong_hu@126.com>
2019-11-24 23:19:31 +0800

    observer pattern

---
+++ 014_ObserverPattern.md
@@ -0,0 +1,2 @@
+
+![youtube-observer-pattern](https://www.youtube.com/watch?v=WRkw0l72BL4)
---
+++ code/u014/v1/Secretary.java
@@ -0,0 +1,7 @@
+package u014.v1;
+
+/**
+ * Created by HuGuodong on 11/24/19.
+ */
+public class Secretary {
+}
